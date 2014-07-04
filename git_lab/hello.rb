852d39e45be4d75f9c23beb406afcce5b6181c7e
cameronjacoby <cameron.jacoby@gmail.com>
2014-07-04 15:16:52 -0700

    create hello.rb

---
+++ git_lab/hello.rb
@@ -0,0 +1,26 @@
+def calc
+  puts "Ruby Calculator \nEnter 'q' to quit."
+
+  while true
+    print ">> "
+    equation = gets.chomp.split(" ")  # splits into array, rejects blanks
+    return if equation[0] == 'q'      # quit if first element is 'q'
+
+    operand1 = equation[0].to_f
+    operand2 = equation[2].to_f
+    operator = equation[1].to_sym
+
+    case operator
+      when :+ then puts operand1 + operand2
+      when :- then puts operand1 - operand2
+      when :* then puts operand1 * operand2
+      when :/ then puts operand1 / operand2
+      when :% then puts operand1 % operand2
+      else
+        puts "invalid input"
+      end
+    end
+
+  end
+
+  calc
